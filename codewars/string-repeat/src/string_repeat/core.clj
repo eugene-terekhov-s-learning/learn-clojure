59bea99b987a96aa28bced42bea2db340383d565
nergal-perm <nergal@psu.ru>
2018-10-04 12:35:12 +0500

    Closes #2. Mine vs. best solution

--- codewars/string-repeat/src/string_repeat/core.clj
+++ codewars/string-repeat/src/string_repeat/core.clj
@@ -1,5 +1,8 @@
 (ns string-repeat.core
   (:gen-class))
 
-(defn repeat-str [n strng])
-  ;; write your code)
+(defn repeat-str [n strng]
+  (reduce #(format "%s%s" %1 %2) (repeat n strng)))
+
+(defn better-repeat-str [n strng]
+  (apply str (repeat n strng)))
