de32dcd92bad51b73312ba7f55d37665c1e952f9
Евгений Терехов <nergal@psu.ru>
2018-10-04 09:07:33 +0500

    Closes #1. My vs. best solution plus full test-suite

--- codewars/sorted-yes-no-how/src/sorted_yes_no_how/core.clj
+++ codewars/sorted-yes-no-how/src/sorted_yes_no_how/core.clj
@@ -1,25 +1,21 @@
 (ns sorted-yes-no-how.core
   (:gen-class))
 
-(defn analyze-sort-type
-  [sq]
-  (loop [remaining-numbers sq
-         analysis-result []]
-    (if (empty? remaining-numbers)
-      (reduce + analysis-result)
-      (recur (rest remaining-numbers)
-        (conj analysis-result 
-          (compare (first remaining-numbers) (second remaining-numbers)))))))
-
-        
-
 (defn sorted-and-how? 
   [sq]
-  (let [sort-type (analyze-sort-type sq)]
-    (println (format "Sort-type: %d, num count: %d" sort-type (count sq)))
-    (if (= sort-type (count sq))
-      "yes, ascending"
-      (if (= sort-type (* (count sq) -1))
-        "yes, descending"
-        "no"))))
+  (let [initial-number-array (seq sq)]
+    (if (= (count (set sq)) (count sq))
+      (if (= (sort sq) initial-number-array)
+        "yes, ascending"
+        (if (= (sort-by - sq) initial-number-array)
+          "yes, descending"
+          "no"))
+      "no")))
+
+(defn better-sorted-and-how?
+  [sq]
+  (cond
+    (apply < sq) "yes, ascending"
+    (apply > sq) "yes, descending"
+    :else "no"))
   
--- codewars/sorted-yes-no-how/test/sorted_yes_no_how/core_test.clj
+++ codewars/sorted-yes-no-how/test/sorted_yes_no_how/core_test.clj
@@ -3,8 +3,47 @@
             [sorted-yes-no-how.core :refer :all])
   (use [sorted-yes-no-how.core :rename {sorted-and-how? solution}]))
 
-(deftest sample-tests
+(declare rand-input
+  rand-int-array
+  rand-sample
+  master-solution)
+
+(deftest basic-tests
   (are [inp exp] (= exp (solution inp))
        [1 2] "yes, ascending"
        [15 7 3 -8] "yes, descending"
        [4 2 30] "no"))
+
+(deftest random-tests
+  (let [rand-test-count 32]
+       (dotimes [_n rand-test-count]
+                (let [input    (rand-input)
+                      actual   (solution input)
+                      expected (master-solution input)]
+                     (do (println "Input: " input)
+                         (println (str "Expected: \"" expected "\""))
+                         (println (str "Actual:   \"" actual "\""))
+                         (is (= actual expected)))))))
+
+(defn- rand-input []
+  (let [ary (rand-int-array)]
+       (if (= 1 (count (distinct ary))) (rand-input)
+           (let [mutate (rand-sample [identity sort (comp reverse sort)])]
+                (mutate ary)))))
+
+(defn- rand-int-array []
+  (let [max-cnt 16
+        max-val 255
+        cnt     (max 2 (rand-int (inc max-cnt)))]
+       (->> (repeatedly #(rand-int (inc max-val)))
+            (take cnt))))
+
+(defn- rand-sample [sq]
+  (let [idx (rand-int (count sq))]
+       (nth sq idx)))
+
+(defn- master-solution [sq]
+  (let [pairs (partition 2 1 sq)]
+       (cond (every? (fn [[x y]] (> x y)) pairs) "yes, descending"
+             (every? (fn [[x y]] (< x y)) pairs) "yes, ascending"
+             :else "no")))
