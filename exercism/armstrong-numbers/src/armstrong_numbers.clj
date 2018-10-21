04a58f7b0216299df11af5c695f7a22527f22b67
Евгений Терехов <nergal@psu.ru>
2018-10-21 07:17:29 +0500

    Added IDEA files to gitignore, minor cosmetic fixes

--- exercism/armstrong-numbers/src/armstrong_numbers.clj
+++ exercism/armstrong-numbers/src/armstrong_numbers.clj
@@ -4,7 +4,7 @@
   "Calculates power of a given number"
   [num power]
   (reduce * (take power (repeat num))))
-  
+
 
 (defn armstrong
   "Calculates armstrong number"
@@ -12,15 +12,14 @@
   (let [num-as-string (str num)
         count (count num-as-string)]
     (reduce +
-      (map #(power % count) 
-        (map #(Integer/parseInt %) (map str (seq num-as-string)))))))
-    
-  
+            (map #(power % count)
+                 (map #(Integer/parseInt %) (map str (seq num-as-string)))))))
+
+
 
 (defn armstrong? [num]
-  (let [count 1]
-    (=
-       num
-       (armstrong num))))
+  (=
+    num
+    (armstrong num)))
     
   
