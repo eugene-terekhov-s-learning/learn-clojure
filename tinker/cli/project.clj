46f74fc5fc22b52563035b9171fdb885de287ea1
nergal-perm <nergal@psu.ru>
2018-11-13 10:26:54 +0500

    Added template for a CLI application

---
+++ tinker/cli/project.clj
@@ -0,0 +1,6 @@
+(defproject cli-parser "0.1.0-SNAPSHOT"
+  :description "try to parse command line arguments"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.4.1"]]
+  :main command)
+
